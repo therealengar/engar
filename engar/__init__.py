6a983acaec114868204f6a87187256235b9e0214
mesik <71527914+mesiik@users.noreply.github.com>
2022-10-23 01:39:02 +0200

    hehehehaw

---
+++ engar/__init__.py
@@ -0,0 +1,3 @@
+# Hello! if you are wondering, why there is no engar source code here so you can take some commands and add them to your own bot, thats the point.
+# engar is closed source project so hopefully you won't be able to see any copy of it.
+# any way have a nice day/night
